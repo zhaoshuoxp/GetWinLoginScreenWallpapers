c4e7dcfec2fb97176d1d814f5f3d318e83b6c0a6
Quanyi Zhao <zhaoshuoxp@whu.edu.cn>
2021-08-12 16:37:50 +0800

    Create getwp.sh

---
+++ getwp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd  /mnt/c/Users/dell/Pictures/Wallpaper 
+cp /mnt/c/Users/dell/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets/* .
+
+for i in $(ls -1 *|grep -v "jpg"|grep -v png);do
+    size=$(wc -c $i|awk '{print $1}')
+    #echo $size
+    if [ $size -gt 100000 ];then
+        mv $i ${i}.png
+    else
+        rm $i
+    fi
+done
